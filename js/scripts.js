059812d3af6a1bd87a3b094ad299909ff6153638
trrapp@gmail.com <trrapp@gmail.com>
2016-10-17 18:08:16 -0600

    chore(init) Add .gitignore, html, js, css

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+/* https://open.whitehouse.gov/ data from the
+   Climate Change Adaptation Task Force
+*/
+
+var ourData = {};
+
+window.onload = function climateChange () {
+
+  let url = 'https://open.whitehouse.gov/resource/ybwj-5tg8.json'
+
+  // over 27,000 hits with the following
+  // $.get(url, function(data) {
+  //   console.log(data);
+  // });
+
+  // we'll do the long version to prevent overload
+  $.ajax({
+    dataType: "json",
+    url: url,
+    data: {
+      "$limit" : 100
+    }
+  }).done(function(data) {
+    ourData = data;
+  })
+
+};
